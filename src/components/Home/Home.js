0505191be707770d0b8aab944578b0f68487afaf
Georgi Stoilkov <gogost6@gmail.com>
2022-03-22 15:36:20 +0200

    some vanila for scroll

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,27 @@
 import "./Home.scss";
 
 const Home = () => {
+    
+    const onClick = (e) => {
+        e.preventDefault();
+        document.body.scrollTop = 452;
+        document.documentElement.scrollTop = 452;
+    }
+
     return (
         <>
-            <div>
-                <img className="landing-img" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp3160287.jpg&f=1&nofb=1" alt="ff9" />
+            <div className="home">
+                <button className="btn" onClick={onClick}>Go to favourites</button>
             </div>
-            <h3 style={{textAlign: 'center', marginTop: '15px'}}>Favorites</h3>
-            <ul></ul>
+            <div className="favorites-container">
+                <h2>My Favorites</h2>
+                <div className="img-container">
+                    <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CBGvX0Yqcc7nWM3hxqhQGQHaEo%26pid%3DApi&f=1" alt="img" />
+                    <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CBGvX0Yqcc7nWM3hxqhQGQHaEo%26pid%3DApi&f=1" alt="img" />
+                    <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CBGvX0Yqcc7nWM3hxqhQGQHaEo%26pid%3DApi&f=1" alt="img" />
+                    <img width="200px" height="200px" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.CBGvX0Yqcc7nWM3hxqhQGQHaEo%26pid%3DApi&f=1" alt="img" />
+                </div>
+            </div>
         </>
     );
 }

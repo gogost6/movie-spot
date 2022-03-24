ed1a1bf8fb69c649e2ee78c751449640442f298a
Georgi Stoilkov <gogost6@gmail.com>
2022-03-24 17:37:30 +0200

    user persist and route guard

--- src/App.js
+++ src/App.js
@@ -6,16 +6,35 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import Results from './components/Results/Results';
 import Details from './components/Details/Details';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getUser } from './services/authServices';
+import { userAuthentication } from './features/user/userSlice';
+import LoggedUserGuard from './guards/LoggedUserGuard';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getUser()
+      .then(res => {
+        dispatch(userAuthentication(res));
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, [])
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>} />
-        <Route path='/search' element={<Results/>}/>
+        <Route path='/' element={<Home />} />
+        <Route element={<LoggedUserGuard />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Route>
+        <Route path='/search' element={<Results />} />
         <Route path='/movies/*' element={<Details />} />
       </Routes>
     </>

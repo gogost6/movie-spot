6b04965ccecdd8d57ee44f1d52eadf353fc4e9f2
Georgi Stoilkov <gogost6@gmail.com>
2022-03-24 17:38:00 +0200

    dinamic header update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,27 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { userLogout } from "../../features/user/userSlice";
+import { logoutHandled } from "../../services/authServices";
 import Search from "../Search/Search";
 import "./Header.scss";
 
 const Header = () => {
+    const user = useSelector(state => state.user.value);
+    const dispatch = useDispatch();
+
+    let logoutBtn = () => {
+        logoutHandled().then(res => console.log(res)).catch(err => console.log(err));
+        dispatch(userLogout());
+    }
+
     return (
         <ul className="header">
             <li>Movie Spot Logo</li>
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
+            {user._id
+                ? <li><Link onClick={logoutBtn} to="/">Logout</Link></li>
+                : <><li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li></>}
             <li className="search-li"><Search /></li>
         </ul>
     );

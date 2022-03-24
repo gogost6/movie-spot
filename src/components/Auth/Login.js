46870762903eaaaa1f52bb9bae689c187385ccda
Georgi Stoilkov <gogost6@gmail.com>
2022-03-24 18:55:48 +0200

    controlled form and errors commit

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,17 +1,44 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import "./Auth.scss";
+import { userAuthentication } from "../../features/user/userSlice";
+import { loginUser } from "../../services/authServices";
+import { useState } from "react";
 
 const Login = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [error, setError] = useState();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setError('');
+
+        let formData = new FormData(e.currentTarget);
+        let data = Object.fromEntries(formData);
+
+        loginUser(data)
+            .then(res => {
+                dispatch(userAuthentication(res));
+                navigate('/');
+            })
+            .catch(err => {
+                setError(err);
+                console.log(err);
+            })
+    }
+
     return (
         <div className="auth-container">
-            <form>
+            <form onSubmit={onSubmit}>
                 <h4>User Login</h4>
                 <input className="input" name="username" type="text" placeholder="username" />
-                <input className="input" name="password" type="text" placeholder="password" />
+                <input className="input" name="password" type="password" placeholder="password" />
                 <div className="auth-btn-wrap">
                     <Link to="/register">No registration?</Link>
                     <button>Login</button>
                 </div>
+                {error ? <p className="error hide" style={{color: 'red'}}>Wrong username or password</p> : ''}
             </form>
         </div>
     );
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,18 +1,89 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { userAuthentication } from "../../features/user/userSlice";
+import { usedEmail, usedUsername, registerUser } from "../../services/authServices";
 import "./Auth.scss";
 
 const Register = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    let [isSubmitted, setIsSubmitted] = useState(false);
+    let [username, setUsername] = useState('');
+    let [usedUsernameState, setUsedUsernameState] = useState(false);
+    let [usedEmailState, setUsedEmailState] = useState(false);
+    let [email, setEmail] = useState('');
+    let [password, setPassword] = useState('');
+    let [repeatPassword, setRepeatPassword] = useState('');
+
+    const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setIsSubmitted(true);
+
+        let formData = new FormData(e.currentTarget);
+        let data = Object.fromEntries(formData);
+
+        if (data.username.length > 4) {
+            usedUsername(data.username)
+                .then(res => setUsedUsernameState(res))
+                .catch(err => console.log(err))
+        }
+
+        if (emailRegex.test(data.email)) {
+            usedEmail(data.email)
+                .then(res => setUsedEmailState(res))
+                .catch(err => console.log(err))
+        }
+
+        registerUser(data)
+            .then(res => {
+                dispatch(userAuthentication(res));
+                navigate('/');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    };
+
     return (
         <div className="auth-container">
-            <form>
+            <form onSubmit={onSubmit}>
                 <h4>User Register</h4>
-                <input className="input" name="username" type="text" placeholder="username" />
-                <input className="input" name="password" type="text" placeholder="password" />
-                <input className="input" name="repeat-password" type="text" placeholder="repeat-password" />
+                <input className="input" name="email" type="text"
+                    placeholder="example@gmail.com"
+                    value={email}
+                    onChange={(e) => { setEmail(e.target.value) }} />
+                {email === '' && isSubmitted ? <p className="error">Email is empty!</p> : ''}
+                {!emailRegex.test(email) && isSubmitted ? <p className="error">Email is invalid!</p> : ''}
+                {usedEmailState ? <p className="error">Email is used by other user!</p> : ''}
+                <input className="input" name="username" type="text"
+                    placeholder="username"
+                    value={username}
+                    onChange={(e) => { setUsername(e.target.value) }} />
+                {username === '' && isSubmitted ? <p className="error">Username is empty!</p> : ''}
+                {username.length < 5 && isSubmitted ? <p className="error">Username should be atleast 5 symbols!</p> : ''}
+                {usedUsernameState ? <p className="error">Username is used by other user!</p> : ''}
+                <input className="input" name="password" type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => { setPassword(e.target.value) }} />
+                {password === '' && isSubmitted ? <p className="error">Password is empty!</p> : ''}
+                {password.length < 4 && isSubmitted ? <p className="error">Password should be atleast 4 symbols!</p> : ''}
+                <input className="input" name="repassword" type="password"
+                    placeholder="repeat password"
+                    value={repeatPassword}
+                    onChange={(e) => { setRepeatPassword(e.target.value) }} />
+                {repeatPassword === '' && isSubmitted ? <p className="error">Repeat password is empty!</p> : ''}
+                {password !== repeatPassword && isSubmitted ? <p className="error">Password and repeat password don't match!</p> : ''}
                 <div className="auth-btn-wrap">
                     <Link to="/login">Login?</Link>
                     <button>Register</button>
                 </div>
+                <p className="warning">*username should be atleast 5 symbols</p>
+                <p className="warning">*password should be atleast 4 symbols</p>
             </form>
         </div>
     );

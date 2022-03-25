13121bf7bbed4ad29b68d8aca7070a7b2b7060e1
Georgi Stoilkov <gogost6@gmail.com>
2022-03-25 15:00:29 +0200

    get popular request added

--- src/services/openApiServices.js
+++ src/services/openApiServices.js
@@ -26,3 +26,18 @@ export const getMovieDetails = async (id) => {
         return result;
     }
 }
+
+export const getPopular = async (id) => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=f6451d1e65623a59357d3ebb3f197431&language=en-US&page=1`, {
+        method: 'GET',
+    })
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    } else {
+        return result;
+    }
+}
+

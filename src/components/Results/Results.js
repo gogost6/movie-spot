b1024851a19cff4791cfecf78ff473be2d9a323c
Georgi Stoilkov <gogost6@gmail.com>
2022-03-25 14:12:42 +0200

    request commit

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,14 +1,28 @@
 import "./Results.scss";
 import Search from "../Search/Search";
 import Card from "../Card/Card";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { getMovieByTitle } from "../../services/openApiServices";
+import { useDispatch, useSelector } from "react-redux";
+import { addMovies } from "../../features/user/moviesSlice";
 
 const Results = () => {
+    const location = useLocation();
+    const dispatch = useDispatch();
+
+    let filmTitle = location.search.split('=')[1];
+
+    useEffect(() => {
+        getMovieByTitle(filmTitle)
+            .then(res => dispatch(addMovies(res.results)))
+            .catch(err => console.log(err))
+    }, [location])
+
     return (
         <div className="results-container">
             <Search />
-            <Card />
-            <Card />
-            <Card />
+            <Card details={false}/>
         </div>
     );
 }

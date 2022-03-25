409c1f92c2ae0f5f8221f3cb9d79370fc4bfab3b
Georgi Stoilkov <gogost6@gmail.com>
2022-03-25 14:11:05 +0200

    api update

--- src/services/openApiServices.js
+++ src/services/openApiServices.js
@@ -1,13 +1,21 @@
-let baseUrl = 'http://www.omdbapi.com/?apikey=da80ad9b&';
-
 export const getMovieByTitle = async (title) => {
-    //title should have "+" not " "
-    const response = await fetch(`${baseUrl}${title}`, {
+    // title should have "+" not " "
+    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=f6451d1e65623a59357d3ebb3f197431&query=${title}&page=1`, {
         method: 'GET',
-        headers: {
-            'content-type': 'application/json'
-        },
-        credentials: 'include'
+    })
+
+    const result = await response.json();
+
+    if (!response.ok) {
+        throw result;
+    } else {
+        return result;
+    }
+}
+
+export const getMovieDetails = async (id) => {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=f6451d1e65623a59357d3ebb3f197431`, {
+        method: 'GET',
     })
 
     const result = await response.json();

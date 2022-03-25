009cb34b271afb0104473ab44f65e0b12eb36686
Georgi Stoilkov <gogost6@gmail.com>
2022-03-25 14:12:15 +0200

    details request added

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,10 +1,25 @@
 import "./Details.scss";
 import Card from "../Card/Card.js";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { getMovieDetails } from "../../services/openApiServices";
+import { useDispatch } from "react-redux";
+import { addMovieDetails } from "../../features/user/moviesSlice";
 
 const Details = () => {
+    const location = useLocation();
+    const dispatch = useDispatch();
+    const id = location.pathname.split('/')[2];
+    
+    useEffect(() => {
+        getMovieDetails(id)
+            .then(res => dispatch(addMovieDetails(res)))
+            .catch(err => console.log(err))
+    }, [id])
+    
     return (
         <div style={{ margin: '5% 10%' }}>
-            <Card />
+            <Card details={true}/>
             <div className="review">
                 <h2>Your Review</h2>
                 <h1>* * * * *</h1>

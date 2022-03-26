7be8eb523f8098cf3d5b5aa5ad7e9c4240bd19e0
Georgi Stoilkov <gogost6@gmail.com>
2022-03-26 18:05:57 +0200

    Results -> favorite btn works

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,30 +1,90 @@
 import "./Results.scss";
 import Search from "../Search/Search";
-import Card from "../Card/Card";
-import { useLocation } from "react-router-dom";
-import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { getMovieByTitle } from "../../services/openApiServices";
 import { useDispatch, useSelector } from "react-redux";
 import { addMovies } from "../../features/user/moviesSlice";
+import { addMovie, favoriteAdd, favoriteRemove } from "../../services/moviesServices";
+import { addFavoriteRedux, pushMovie, removeFavoriteRedux } from "../../features/user/userSlice";
 
 const Results = () => {
     const location = useLocation();
     const dispatch = useDispatch();
-
+    const user = useSelector(state => state.user.value);
     let filmTitle = location.search.split('=')[1];
+    const searchedMovies = useSelector(state => state.movies.value.searchedMovies);
 
     useEffect(() => {
         getMovieByTitle(filmTitle)
-            .then(res => dispatch(addMovies(res.results)))
-            .catch(err => console.log(err))
-    }, [location])
+            .then(res => {
+                const newArray = res.results.map(x =>
+                    user.movies.find(y =>
+                        y.name === x.original_title && y.favorite === true)
+                        ? { ...x, favorite: true }
+                        : x)
+                dispatch(addMovies(newArray))
+            })
+            .catch(err => console.log(err));
 
-    return (
-        <div className="results-container">
+    }, [location, user.movies])
+
+    const addToFavoriteBtn = (e, movie) => {
+        e.preventDefault();
+        const foundedIndex = user.movies.findIndex(x => x.name == movie.original_title);
+        if (foundedIndex !== -1) {
+            favoriteAdd({ email: user.email, name: movie.original_title })
+                .then(res => console.log(res))
+                .catch(err => console.log(err));
+
+            dispatch(addFavoriteRedux(foundedIndex));
+        } else {
+            const reqData = {
+                name: movie.original_title,
+                rating: 0,
+                favorite: true,
+                notes: '',
+                poster_path: movie.poster_path,
+                id: movie.id
+            }
+
+            addMovie({ email: user.email, ...reqData })
+                .then(res => console.log(res))
+                .catch(err => console.log(err));
+
+            dispatch(pushMovie(reqData))
+        }
+    }
+
+    const removeFromFavoriteBtn = (e, name) => {
+        const foundedIndex = user.movies.findIndex(x => x.name == name);
+
+        favoriteRemove({ email: user.email, name })
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
+        dispatch(removeFavoriteRedux(foundedIndex));
+    }
+
+    if (searchedMovies && searchedMovies.length > 0) {
+        return <div className="results-container">
             <Search />
-            <Card details={false}/>
+            {searchedMovies.map(x => <div className="card" key={x.id}>
+                <Link to={`/movies/${x.id}`} className="card-img-wrap">
+                    <img src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2${x.poster_path}`} alt="img" />
+                </Link>
+                <div className="card-content">
+                    <h2>{x.original_title} ({x.release_date.split('-')[0]})</h2>
+                    <p>{x.overview}</p>
+                    <Link to={`/movies/${x.id}`}>Link to details</Link>
+                    {x.favorite
+                        ? <button onClick={e => removeFromFavoriteBtn(e, x.original_title)} >Remove from favourite</button>
+                        : <button onClick={e => addToFavoriteBtn(e, x)} >Add to favourite</button>}
+                </div>
+            </div>)}
         </div>
-    );
+    } else {
+        return (<p>Error</p>);
+    }
 }
 
 export default Results;

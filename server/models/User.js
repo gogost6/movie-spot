d20afbe751703274f7781f0a3c0a2df3404b0b08
Georgi Stoilkov <gogost6@gmail.com>
2022-03-26 12:39:51 +0200

    server -> id and poster commit in model

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const schema = {
     email: { type: String, required: true },
     username: { type: String, required: true },
     hashedPassword: { type: String, required: true },
-    movies: [{name: String, rating: Number, notes: String, favorite: Boolean}],
+    movies: [{name: String, rating: Number, notes: String, favorite: Boolean, poster_path: String, id: Number}],
 };
 
 module.exports = model('User', schema);
